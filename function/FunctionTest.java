0d5ac167dc997553489b08a8b99251f6b64fa2b6
ZOLTAN Krizsan <krizsan@gmail.com>
2021-10-12 14:42:54 +0200

    Update FunctionTest.java

--- function/FunctionTest.java
+++ function/FunctionTest.java
@@ -1,23 +1,29 @@
 class FunctionTest {
+  Stream<String> cities = Stream.of("Tokio", "New York", "Miskolc", "Marokko", "Mo");
+  
   @Test
   public void ConsumerTest(){
       Consumer<String> printConsumer = t -> System.out.println(t);
-      Stream<String> cities = Stream.of("Tokio", "New York", "Miskolc");
       cities.forEach(printConsumer);
   }
   
   @Test
   public void supplierTest(){
       Supplier<Double> doubleSupplier = () -> Math.random();
-
       System.out.println(doubleSupplier.get());
   }
   
   @Test
   public void predicateTest(){
-      List<String> names = Arrays.asList("John", "Johan", "Blue", "Joe", "Siri");
-      Predicate<String> startPredicate = str -> str.startsWith("J");
+      Predicate<String> startPredicate = str -> str.startsWith("M");
       Predicate<String> lengthPredicate = str -> str.length() >= 3;
-      names.stream().filter(startPredicate.and(lengthPredicate)).forEach(System.out::println);
+      cities.filter(startPredicate.and(lengthPredicate)).forEach(System.out::println);
+  }
+  
+  @Test
+  public void testFunction(){
+      Function<String, Integer> nameMappingFunction = String::length;
+      List<Integer> nameLengthList = cities.map(nameMappingFunction).collect(Collectors.toList());
+      System.out.println(nameLength);
   }
 }
